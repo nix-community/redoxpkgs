a989957d8d35b49c87372d5ae936b6d960af1e9a
Aaron Janse <aaron@ajanse.me>
2020-07-31 10:51:18 -0700

    try pinning again

--- default.nix
+++ default.nix
@@ -1,10 +1,7 @@
 let
-  bootstrapNixpkgs = import <nixpkgs> {};
-  nixpkgs = import (bootstrapNixpkgs.fetchFromGitHub {
-    owner = "NixOS";
-    repo = "nixpkgs";
+  nixpkgs = import (fetchGit {
+    url = "https://github.com/NixOS/nixpkgs";
     rev = "5c56778efdcaa1b8088eb536c3f1e9cc110930dc";
-    sha256 = "0j6yhlnvshj24zk6liszwisfnc4cplhsibb7hwg18f5wmaj4alc9";
   });
   overlay = import ./overlay;
 in
