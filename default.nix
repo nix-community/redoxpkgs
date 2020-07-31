3470f1b242c1db8b07ed656523722f693dfd48a9
Aaron Janse <aaron@ajanse.me>
2020-07-31 10:49:14 -0700

    re-pin nixpkgs

--- default.nix
+++ default.nix
@@ -1,15 +1,17 @@
 let
-  parentNixpkgs = import (fetchGit {
-    url = "https://github.com/aaronjanse/nixpkgs";
-    ref = "aj-redox";
+  bootstrapNixpkgs = import <nixpkgs> {};
+  nixpkgs = import (bootstrapNixpkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "5c56778efdcaa1b8088eb536c3f1e9cc110930dc";
+    sha256 = "0j6yhlnvshj24zk6liszwisfnc4cplhsibb7hwg18f5wmaj4alc9";
   });
-  # parentNixpkgs = import ../rixpkgs;
   overlay = import ./overlay;
 in
 
-with (parentNixpkgs {
-  overlays = [ overlay ];
-  config.allowUnsupportedSystem = true;
+with (nixpkgs {
+    overlays = [ overlay ];
+    config.allowUnsupportedSystem = true;
 }); pkgsCross.x86_64-unknown-redox // {
   origPkgs = pkgs;
 }
