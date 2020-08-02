07537399559a2553ad6f2414761bf598a9a7ada2
Aaron Janse <aaron@ajanse.me>
2020-08-02 01:52:21 -0700

    use fetchTarball for nixpkgs

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let
-  nixpkgs = import (fetchGit {
-    url = "https://github.com/NixOS/nixpkgs";
-    rev = "64a9b4b7a341c7f423932bf8f4366f07654060e6";
+  nixpkgs = import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/64a9b4b7a341c7f423932bf8f4366f07654060e6.tar.gz";
+    sha256 = "0fhamzazlnf78chlcxg84q3kbjmg10srvp0vmn4kkmy449cjhw5j";
   });
   overlay = import ./overlay;
 in
