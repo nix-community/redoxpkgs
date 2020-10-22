836c07dfefaa8bf2b61da258ffdc6c15c1f004df
Aaron Janse <aaron@ajanse.me>
2020-10-21 18:09:43 -0700

    add gcc to hydra

--- .hydra/default.nix
+++ .hydra/default.nix
@@ -1,12 +1,12 @@
 let
   pkgs = import ../default.nix;
-in {}
-  # inherit (pkgs.pkgsCross.x86_64-unknown-redox.buildPackages) gcc rustc;
+in {
+  inherit (pkgs.pkgsCross.x86_64-unknown-redox.buildPackages) gcc; # rustc;
   # inherit (pkgs.pkgsCross.x86_64-unknown-redox)
   #   hexyl bash less vim
   #   perl cmatrix cowsay
   #   binutils-unwrapped
   #   SDL2 pcre ncurses
   #   # llvm rcoreutils python37
-  # ;
-
+  ;
+}
