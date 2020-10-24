177682503ffaa6113048c177146255d61885ae64
Aaron Janse <aaron@ajanse.me>
2020-10-24 00:02:45 -0700

    enable more packages

--- .hydra/default.nix
+++ .hydra/default.nix
@@ -1,9 +1,9 @@
 let
   pkgs = import ../default.nix;
 in {
-  inherit (pkgs.pkgsCross.x86_64-unknown-redox.buildPackages) gcc; # rustc;
-  # inherit (pkgs.pkgsCross.x86_64-unknown-redox)
-  #   hexyl bash less vim
+  inherit (pkgs.pkgsCross.x86_64-unknown-redox.buildPackages) gcc rustc;
+  inherit (pkgs.pkgsCross.x86_64-unknown-redox)
+     hexyl bash less vim;
   #   perl cmatrix cowsay
   #   binutils-unwrapped
   #   SDL2 pcre ncurses
